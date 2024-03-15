2c3fca293854e6a9203acdc9c9f351afd0ddf71a
gexiaoqian <3506466428@qq.com>
2024-03-16 04:56:05 +0800

    feat: avatar: change font-size based on size props

--- packages/App.vue
+++ packages/App.vue
@@ -1,22 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-
-const img = ref('https://empty')
-
-const onError = () => {
-  img.value = 'https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png'
-}
 </script>
 
 <template>
   <div class="container">
-    <FrAvatar
-      :size="100"
-      shape="square"
-      :src="img"
-      fit="scale-down"
-      @error="onError"
-    ></FrAvatar>
+    <FrAvatar>
+      user
+    </FrAvatar>
   </div>
 </template>
 
