44cddcb200243af785fd44f3d2939732d9728e4f
zlsk1 <3506466428@qq.com>
2024-07-20 05:31:31 +0800

    fix: [docs] fix position mistake in vp-nav component

--- docs/.vitepress/theme/components/vp-nav.vue
+++ docs/.vitepress/theme/components/vp-nav.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="nav-container">
-    <a href="">Nocturne-ui</a>
-    <div class="content">
-      <ul class="nav-link">
-        <li v-for="(item, index) in theme.nav" :key="item.text">
-          <a :href="item.link" :class="actived(index) ? 'actived' : ''">{{ item.text }}</a>
-        </li>
-      </ul>
+    <div class="nav-wrapper">
+      <a href="">Nocturne-ui</a>
+      <div class="content">
+        <ul class="nav-link">
+          <li v-for="(item, index) in theme.value.nav" :key="item.text">
+            <a :href="item.link" :class="actived(index) ? 'actived' : ''">{{ item.text }}</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
 import { useData, useRoute } from 'vitepress'
 
-const { theme, page, site } = useData()
+const { theme } = useData()
 const route = useRoute()
 
-const actived = index => {
+const actived = (index: number) => {
   const item = theme.value.nav[index]
   return item.link + '.html' === route.path
 }
