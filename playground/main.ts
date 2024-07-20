6ba874c84f25f6900bb06168b3bebed417c262e7
zlsk1 <3506466428@qq.com>
2024-07-20 23:45:10 +0800

    fix: [button] resolve border-radius bug when use circle

--- playground/main.ts
+++ playground/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './src/App.vue'
-import NocturneUI from '../dist/nocturne-ui/dist/index.full'
-import '../dist/nocturne-ui/dist/index.css'
+import NocturneUI from 'nocturne-ui/index'
+import 'nocturne-ui/theme-chalk/src/index.scss'
 
 const app = createApp(App)
 
