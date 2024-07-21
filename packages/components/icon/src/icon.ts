5c98d08adc08bf3268a3bd9e81f183b13e87f596
zlsk1 <3506466428@qq.com>
2024-07-21 12:22:37 +0800

    feat: [icon] remove useless prop and add new style

--- packages/components/icon/src/icon.ts
+++ packages/components/icon/src/icon.ts
@@ -1,14 +1,12 @@
 import { definePropType } from '@/utils'
 
-import type { ExtractPropTypes, StyleValue } from 'vue'
+import type { ExtractPropTypes } from 'vue'
 
 export const iconProps = {
   size: {
     type: definePropType<string | number>([String, Number])
   },
-  color: String,
-  // 图标类名
-  className: definePropType<StyleValue>([String, Array])
+  color: String
 }
 
 export type IconProps = ExtractPropTypes<typeof iconProps>
--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -1,44 +1,19 @@
-<!-- eslint-disable vue/no-multiple-template-root -->
 <template>
-  <i
-    v-if="$slots.default"
-    class="n-icon"
-    :class="svgClass"
-    :style="svgStyle"
-  >
+  <i class="n-icon" :style="svgStyle">
     <slot></slot>
   </i>
-  <template v-else>
-    <slot></slot>
-  </template>
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue'
 import { iconProps } from './icon'
-import { isString } from '@/utils'
-import type { StyleValue, CSSProperties } from 'vue'
+import type { CSSProperties } from 'vue'
 
 defineOptions({
   name: 'NIcon'
 })
 
 const props = defineProps(iconProps)
-
-const svgClass = computed<StyleValue>(() => {
-  if (isString(props.className)) {
-    return 'n-icon ' + props.className
-  }
-  else if (Array.isArray(props.className)) {
-    const className = props.className.map(v => {
-      return v
-    }).join(' ')
-    return 'n-icon ' + className
-  }
-  else {
-    return 'n-icon'
-  }
-})
 
 const svgStyle = computed<CSSProperties>(() => {
   return {
