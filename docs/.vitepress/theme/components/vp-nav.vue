4f79ab6284f03efa6f6710b6696e1414ec7e9714
zlsk1 <3506466428@qq.com>
2024-07-21 10:56:29 +0800

    docs: add github link and view switch in vp-nav

--- docs/.vitepress/theme/components/vp-nav.vue
+++ docs/.vitepress/theme/components/vp-nav.vue
@@ -3,11 +3,13 @@
     <div class="nav-wrapper">
       <a href="">Nocturne-ui</a>
       <div class="content">
-        <ul class="nav-link">
-          <li v-for="(item, index) in theme.nav" :key="item.text">
-            <a :href="item.link" :class="{ 'actived': actived(theme.nav[index].link) }">{{ item.text }}</a>
-          </li>
-        </ul>
+        <nav class="nav-link">
+          <a v-for="(item, index) in theme.nav" :key="item.text" :href="item.link" :class="{ 'actived': actived(theme.nav[index].link) }">{{ item.text }}</a>
+        </nav>
+        <n-switch></n-switch>
+        <a href="https://github.com/zlsk1/nocturne-ui" class="link-github" title="github" target="_blank">
+          <Github></Github>
+        </a>
       </div>
     </div>
   </div>
@@ -15,6 +17,7 @@
 
 <script lang="ts" setup>
 import { useData, useRoute } from 'vitepress'
+import { RiGithubFill as Github } from '@remixicon/vue';
 
 const { theme } = useData()
 const route = useRoute()
@@ -26,6 +29,3 @@ const actived = (link: string) => {
   return route.path.includes(joinlink)
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
