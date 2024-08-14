67af6b017084d1e698055525ab65133985f21fe6
zlsk1 <3506466428@qq.com>
2024-08-15 06:35:14 +0800

    docs: can display anchor links in doc-content

--- docs/.vitepress/theme/components/vp-doc-content.vue
+++ docs/.vitepress/theme/components/vp-doc-content.vue
@@ -1,20 +1,36 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
-import { useData } from 'vitepress'
+import { onMounted, watch, nextTick } from 'vue';
+import { useData, useRoute } from 'vitepress'
 import vpAPIAside from './vp-api-aside.vue' 
 import vpPageSwitch from './vp-page-switch.vue'
 import vpLastUpdate from './vp-last-update.vue'
+
+import type { PageData } from 'vitepress'
 
 onMounted(() => {
   resolveZeroWidthSpace()
 })
 
 const { page } = useData()
+const route = useRoute()
 
 const resolveZeroWidthSpace = () => {
   const anchor = document.querySelectorAll('.header-anchor')
   Array.from(anchor).forEach(v => v.textContent = '#')
 }
+
+onMounted(() => {
+  watch(
+    () => route.path,
+    async() => {
+      await nextTick()
+      resolveZeroWidthSpace()
+    },
+    {
+      immediate: true,
+    }
+  )
+})
 </script>
 
 <template>
@@ -24,6 +40,6 @@ const resolveZeroWidthSpace = () => {
       <vpLastUpdate></vpLastUpdate>
       <vpPageSwitch></vpPageSwitch>
     </div>
-    <vpAPIAside v-if="page.headers"></vpAPIAside>
+    <vpAPIAside v-if="(page as unknown as PageData).headers"></vpAPIAside>
   </div>
 </template>
