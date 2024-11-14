837ac1c3ffd05445e7556f0ac112a84e1f8c82de
zlsk1 <3506466428@qq.com>
2024-11-15 00:41:52 +0800

    fix(components): [anchor] delete the export of anchor-item

--- packages/components/anchor/index.ts
+++ packages/components/anchor/index.ts
@@ -3,6 +3,6 @@ import anchor from './src/anchor.vue'
 import anchorItem from './src/anchor-item.vue'
 
 export * from './src/anchor'
-export const NAnchor = withInstall(anchor, { anchorItem })
+export const NAnchor = withInstall(anchor)
 export const NAnchorItem = withInstall(anchorItem)
 export default NAnchor
