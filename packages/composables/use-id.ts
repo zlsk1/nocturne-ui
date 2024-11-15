19920a218b2448117161624e298cef9906a28c66
zlsk1 <3506466428@qq.com>
2024-11-16 00:49:29 +0800

    refactor: refactor use-id

--- packages/composables/use-id.ts
+++ packages/composables/use-id.ts
@@ -1,15 +1,40 @@
-import { computed } from 'vue'
+import { computed, getCurrentInstance, inject, unref } from 'vue'
+import { useGetOverRidesNamesapce } from './use-namespace'
+import type { ComputedRef, InjectionKey, MaybeRef } from 'vue'
+import { isClient } from '@/utils'
+
+export type idInjectionContext = {
+  prefix: number
+  suffix: number
+}
+
+export const ID_INJECTION_KEY: InjectionKey<idInjectionContext> = Symbol('id')
 
 const initialId = {
   prefix: Math.floor(Math.random() * 10000),
   suffix: 0
 }
 
-const namespace = 'n-id'
+export const useId = (overrides?: MaybeRef<string>): ComputedRef<string> => {
+  const injection =
+    unref(overrides) || getCurrentInstance()
+      ? inject(ID_INJECTION_KEY, initialId)
+      : initialId
 
-export const useId = () => {
+  if (!isClient && injection === initialId) {
+    console.warn(
+      `ID_INJECTION_KEY, if you are in SSR enviroment, make sure to provide a id provider by: 
+        app.provide(ID_INJECTION_KEY, {
+          prefix: number
+          suffix: number
+        })
+      `
+    )
+  }
+  const namespace = useGetOverRidesNamesapce()
+
   const id = computed(() => {
-    return `${namespace}--${initialId.prefix}--${initialId.suffix++}`
+    return `${namespace.value}--${injection.prefix}--${injection.suffix++}`
   })
 
   return id
