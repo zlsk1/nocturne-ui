e00f1ba4e79ead057738019abd762d766c143bd0
zlsk1 <3506466428@qq.com>
2025-02-10 02:54:01 +0800

    chore: remove useless comments

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -30,18 +30,18 @@ module.exports = {
       2,
       'always',
       [
-        'feat', // 增加新的特征、功能
-        'fix', // 修复bug
-        'perf', // 提高性能的代码更改
-        'style', // 不影响代码含义的修改，比如空格、格式化、缺失的分号等
-        'docs', // 对文档进行了修改
-        'test', // 增加确实的测试或者矫正已存在的测试
-        'refactor', // 既不是修复bug也不是添加特征的代码重构
-        'build', // 对构建系统或者外部依赖项进行了修改
-        'ci', // 对CI配置文件或脚本进行了修改
-        'chore', // 非功能性变更或对代码库进行的一般维护工作
-        'revert', // 回滚操作, this revert commit hash ...
-        'release' // 发布
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'release'
       ]
     ]
   }
